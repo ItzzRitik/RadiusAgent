aa4b2f33068ad8bbfb036ec98c8bc37b0d24056b
Ritik Srivastava <ritik.space@gmail.com>
2019-04-08 23:50:11 +0530

    User's Object

--- app/src/main/java/com/radiusagent/radius/MainActivity.java
+++ app/src/main/java/com/radiusagent/radius/MainActivity.java
@@ -5,10 +5,13 @@ import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.WindowManager;
@@ -21,6 +24,20 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
     ImageView icosplash;
     RelativeLayout splash;
@@ -31,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     TextView namesplash;
     RotateAnimation rotate;
     AppBarLayout appbar;
+    OkHttpClient client;
     boolean loading=false,splashEND=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +68,43 @@ public class MainActivity extends AppCompatActivity {
         splash();
     }
     public void splash(){
+        Request request = new Request.Builder().url("https://raw.githubusercontent.com/iranjith4/radius-intern-mobile/master/users.json").get()
+                .addHeader("Content-Type", "application/json").build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.w("failure", e.getMessage());
+                call.cancel();
+            }
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                assert response.body() != null;
+                String mMessage = Objects.requireNonNull(response.body()).string();
+                if (response.isSuccessful()){
+                    try {
+                        JSONArray postsArray = new JSONArray(mMessage);
+                        schemes = new ArrayList<>();
+                        for (int i = 0; i < postsArray.length(); i++) {
+                            JSONObject pO = postsArray.getJSONObject(i);
+                            Log.v("error",pO.getString("img"));
+                            schemes.add(new Schemes(pO.getString("name"),pO.getString("endDate")
+                                    ,pO.getString("views"),pO.getString("description"),pO.getString("img")));
+                        }
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(Email==null){Email="aditya.aastha@gmail.com";}
+                                if(Aadhaar==null){Aadhaar="123456789123";}
+                                display.setAdapter(new SchemeAdapter(HomeActivity.this,schemes,Email,Aadhaar));
+                            }
+                        });
+                    }
+                    catch (JSONException e) {
+                        Log.w("error", e.toString());
+                    }
+                }
+            }
+        });
         splash.setBackgroundColor(0);
         icosplash=findViewById(R.id.icosplash);
         namesplash=findViewById(R.id.namesplash);
@@ -76,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         animator = ViewAnimationUtils.createCircularReveal(mainpane,cx,cy,0,(float)diagonal);
         animator.setInterpolator(new AccelerateInterpolator());animator.setDuration(1000);
         mainpane.setVisibility(View.VISIBLE);splash.setElevation(1);mainpane.setElevation(2);animator.start();
-        icosplash.animate().scaleX(20f).scaleY(20f).setDuration(1000).start();
+        icosplash.animate().scaleX(25f).scaleY(25f).setDuration(1000).start();
         new Handler().postDelayed(() -> {
             setLightTheme(false,true);
             appbar.setVisibility(View.VISIBLE);
---
+++ app/src/main/java/com/radiusagent/radius/Users.java
@@ -0,0 +1,25 @@
+package com.radiusagent.radius;
+public class Users {
+    private String name;
+    private String email;
+    private String age;
+    private String gender;
+    private String dp;
+    Users(String name,String email,String age,String gender,String dp) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.dp = dp;
+    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getAge() {return age;}
+    public void setAge(String age) {this.age = age;}
+    public String getGender() {return gender;}
+    public void setGender(String gender) {this.gender = gender;}
+    public String getDp() {return dp;}
+    public void setDp(String thumbnail) {this.dp = dp;}
+}
